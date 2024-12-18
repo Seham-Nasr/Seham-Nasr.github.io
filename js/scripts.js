23a8f010165c123b79ea8e2f83c70e093e541cc8
Seham-Nasr <seham.mohammed.s@gmail.com>
2024-12-18 23:50:41 +0100

    my website

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
     const publications = [
         {
+            title: "IndMask: Inductive Explanation for Multivariate Time Series Black-box Models",
+            authors: "S. Nasr, S. Sikdar",
+            publication: "ECAI conference, 2024",
+            link: "https://paperswithcode.com/paper/indmask-inductive-explanation-for"
+        },
+        {
+            title: "End-to-End Speech Recognition For Arabic Dialects",
+            authors: "S. Nasr, R. Duwairi, M. Quwaider",
+            publication: "Arabian Journal for Science and Engineering, 2023",
+            link: "https://link.springer.com/article/10.1007/s13369-023-07670-7"
+        },
+        {
             title: "LEACH protocol enhancement for increasing WSN lifetime",
             authors: "S. Nasr, M. Quwaider",
             publication: "11th International Conference on Information and Communication Systems, 2020",
             link: "https://ieeexplore.ieee.org/document/9491705/"
         },
         {
-            title: "End-to-End Speech Recognition For Arabic Dialects",
-            authors: "S. Nasr, R. Duwairi, M. Quwaider",
-            publication: "Arabian Journal for Science and Engineering, 2023",
-            link: "https://ouci.dntb.gov.ua/en/works/4OnpP3q7/"
-        },
-        {
             title: "Text-independent speaker recognition using deep neural networks",
             authors: "S. Nasr, M. Quwaider, R. Qureshi",
             publication: "International Conference on Information Technology, 2021",
             link: "https://ieeexplore.ieee.org/document/9491705/"
-        },
-        {
-            title: "IndMask: Inductive Explanation for Multivariate Time Series Black-box Models",
-            authors: "S. Nasr, S. Sikdar",
-            publication: "ECAI conference, 2024",
-            link: "#"
         }
     ];
 
